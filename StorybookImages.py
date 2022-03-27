a40dc1ded5b31ba657346395d861f3f31031f2e5
subha-v <subhavee2@gmail.com>
2022-03-27 14:53:49 -0700

    working pasted images

--- StorybookImages.py
+++ StorybookImages.py
@@ -1,8 +1,11 @@
 from PIL import Image
+import numpy as np
+
 path = "./Images/image1.png"
 
-try: 
-    img  = Image.open("./Images/image1.png") 
-    img.show()
-except IOError:
-    pass
+img  = Image.open("./Images/image1.png") 
+# img.show()
+
+background = Image.open("./Images/backgroundimagereal.jpg")
+background.paste(img)
+background.save('Images/pastedimage.jpg', quality=95)
