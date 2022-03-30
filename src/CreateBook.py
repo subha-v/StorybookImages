9d725f512dbe26fa85ef6023d292d9d6756d3cf8
subha-v <subhavee2@gmail.com>
2022-03-29 21:54:31 -0700

    omg working thingy for the images YAS AYS YAS KWEN

--- src/CreateBook.py
+++ src/CreateBook.py
@@ -1,2 +1,7 @@
+from fnmatch import translate
 from src.Translating import *
 
+def CreateBook(bookArray, firstImageFilePath):
+    translatedbookarray = translate_book(bookArray)
+    
+    return 
--- src/ImageProcessing.py
+++ src/ImageProcessing.py
@@ -1,31 +1,49 @@
-from src.Translating import *
+from concurrent.futures import process
+from Translating import *
 from PIL import Image, ImageDraw, ImageFont
 
-ooktext =  {
-    'page1': 'My name is Simo.',
-    'page2': 'I have four friends.'
-}
+title_font = ImageFont.truetype('./src/fonts/ArialUnicodeMS.ttf', 60)
+hindi_font = ImageFont.truetype('./src/fonts/ArialUnicodeMS.ttf', 60)
 
 
-path = "./Images/image1.png"
-title_font = ImageFont.truetype('PlayfairDisplay-Regular.ttf', 60)
-hindi_font = ImageFont.truetype('ArialUnicodeMS.ttf', 60)
+def processTranslatedImage(imagePath, engText, translatedText):
+    background = Image.open("./Images/backgroundimagereal.jpg")
+    img = Image.open(imagePath)
+    width, height = background.size
+    editable_background = ImageDraw.Draw(background)
+    editable_background.text((1600, 200), engText, (0, 0, 0), font=title_font)
+    editable_background.text((1600, 600), translatedText, (0, 0, 0), font=hindi_font)
+    half = 1.7
+    out = img.resize([int(half * s) for s in img.size])
+    background.paste(out, (60, 60))
+    newimage = background.save('Images/pastedimage.jpg', quality=95)
 
-img1  = Image.open("./Images/image1.png") 
+    return newimage
 
-background = Image.open("./Images/backgroundimagereal.jpg")
-width, height = background.size
-editable_background = ImageDraw.Draw(background)
-editable_background.text((1600,200), booktext['page1'], (0, 0, 0), font=title_font)
+if __name__ == "__main__":
+    processTranslatedImage("./images/book-friends/image-007.jpg", "hallo", "मेरा नाम सिमो है।")
 
-# Calling translation function #
 
-# editable_background.text((1250,600), translatedbooktext[0], (0, 0, 0), font=hindi_font)
 
-# Placeholder Text for testing #
-editable_background.text((1600,600), "मेरा नाम सिमो है।", (0, 0, 0), font=hindi_font)
-# img1.thumbnail((height,height))
-half = 1.3
-out = img1.resize( [int(half * s) for s in img1.size] )
-background.paste(out, (60,60))
-background.save('Images/pastedimage.jpg', quality=95)
+# path = "./Images/image1.png"
+
+# img1 = Image.open("./Images/image1.png")
+
+# background = Image.open("./Images/backgroundimagereal.jpg")
+# width, height = background.size
+# editable_background = ImageDraw.Draw(background)
+# editable_background.text(
+#     (1600, 200), booktext['page1'], (0, 0, 0), font=title_font)
+
+# # Calling translation function #
+
+# # editable_background.text((1250,600), translatedbooktext[0], (0, 0, 0), font=hindi_font)
+
+# # Placeholder Text for testing #
+# editable_background.text(
+#     (1600, 600), "मेरा नाम सिमो है।", (0, 0, 0), font=hindi_font)
+# # img1.thumbnail((height,height))
+# half = 1.3
+# out = img1.resize([int(half * s) for s in img1.size])
+# background.paste(out, (60, 60))
+# background.save('Images/pastedimage.jpg', quality=95)
