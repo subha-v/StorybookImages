972a4de3e0206b266729ece474715ca81c9cbb75
subha-v <subhavee2@gmail.com>
2022-04-02 19:53:47 -0700

    working thingy

--- src/Main.py
+++ src/Main.py
@@ -13,14 +13,33 @@ if __name__ == "__main__":
     
     # the input dialog
     first_image_filepath = simpledialog.askstring(title="Test",
-                                    prompt="What's your first image filepath: ")
+                                    prompt="What is the first image of your book's filepath?: ")
+    first_image_filepath2 = simpledialog.askstring(title="Test",
+                                    prompt="What is the first image of your images flepath?: ")
     number_of_pages = simpledialog.askinteger(title="Test",
-                                    prompt="How many pages are in your book:  ")
-    language = simpledialog.askinteger(title="Test",
-                                    prompt="What language:  ")                                
+                                    prompt="How many pages are in your book?:  ")
+    language = simpledialog.askstring(title="Test",
+                                    prompt="What language code is there for your book?:  ")                                
 
     # check it out
-
+    print(first_image_filepath)
     booktext = ExtractBookText(first_image_filepath, number_of_pages)
-    CreateBook(booktext, first_image_filepath)
+    pdf_list = CreateBook(booktext, first_image_filepath2, language)
+
+    dirname = "./images/translated-book"
+    list_images = (listdir(dirname))
+
+    new_list = []
+
+    for i in range (0,len(list_images)-1):
+        new_list.append("./images/translated-book/" + list_images[i])
+
+    print(new_list)
+
+    pdf = FPDF()
+    # imagelist is the list with all image filenames
+    for image in pdf_list:
+        pdf.add_page()
+        pdf.image(image,0,0,200,150)
+    pdf.output("yourfile.pdf", "F")
 
